dab22d9496450dc89ae2a1705f20626a224f37c2
watcharin <twatcharin90@gmail.com>
2021-02-27 14:33:38 +0700

    update appcom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductItemComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
